d3967694fc034e5faf10198e153a999003d81299
Udaykumar-git-hub <sridhanyareddy.12@gmail.com>
2026-04-24 02:27:24 +0530

    ads

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { tanstackStart } from '@tanstack/react-start/plugin';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
