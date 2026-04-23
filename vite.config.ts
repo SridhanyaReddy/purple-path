65908b2035f25a0c910674dfabca76a88b4b4b5a
Udaykumar-git-hub <sridhanyareddy.12@gmail.com>
2026-04-24 03:22:29 +0530

    das

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
 dotenv.config();
